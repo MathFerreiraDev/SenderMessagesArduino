bcc1c50f80e5afacc474237ae63aec3b634ff9f3
MATHEUS FERREIRA <matheus.ferreira367@etec.sp.gov.br>
2024-05-04 00:26:32 -0300

    Adicionar arquivos de projeto.

---
+++ SenderMessagesArduino/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Insira o número o qual receberá as chamadas:");
+Console.ForegroundColor = ConsoleColor.Yellow;
+string number = Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Qual o intervalo (por horas) em que deseja receber atualizações?:");
+Console.ForegroundColor = ConsoleColor.Yellow;
+double hours = double.Parse(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("---------------------------------------------------");
+
+
+
+    try
+    {
+        System.Diagnostics.Process.Start("http://api.whatsapp.com/send?phone=+" + number + "&text=" + "Teste de mensagem!");
+        Thread.Sleep(30000);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Mensagem enviada com sucesso para "+number);
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("ERROR: " + ex.Message);
+    }
+
+
+//Console.ReadKey();
